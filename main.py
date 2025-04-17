8a583de156de85942ef1df637d890264e5b2c342
Grayson-52 <antonio_rota@outlook.com>
2025-04-17 14:09:33 +0200

    bookbot stage 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        text = f.read()
+    return text
+    
+def main():
+    filepath = input("Please insert your relative filepath: ")
+    book = get_book_text(filepath)
+    print(book)
+
+main()

e0bdc3c446d7401e74a4f41b7b011746707d8140
Grayson-52 <antonio_rota@outlook.com>
2025-04-18 19:41:27 +0200

    Bookbot complete

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+from collections import Counter
+import sys
 from stats import get_book_text
 from stats import word_counter
+from stats import character_count
+from stats import sort_count
 
 def main():
-    filepath = ("books/frankenstein.txt")
-    book = word_counter(filepath)
-    print(f"{book} words found in the document")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = (sys.argv[1])
+    word_count = word_counter(filepath)
+    characters = character_count(filepath)
+    report = sort_count(characters)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char in report:
+        # 'char' is the key while 'count' is the amount
+        print(f"{char['char']}: {char['count']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def get_book_text(filepath):
     with open(filepath) as f:
         text = f.read()
@@ -11,6 +13,23 @@ def word_counter(filepath):
     return count
 
 def character_count(filepath):
-    dict = {}
+    characters = {}
     with open(filepath) as f:
-        text = f.lower()
+        book = f.read().lower()
+        chars = [x for x in book]
+        for character in chars:
+            if character not in characters:
+                characters[character] = 0
+            characters[character] += 1
+    return characters
+
+def sort_count(characters):
+    chars = []
+    for char, count in characters.items():
+        if char.isalpha():
+            char_info = {"char": char, "count": count}
+            chars.append(char_info)
+    def sort_on(dict):
+        return dict["count"]
+    chars.sort(reverse=True, key=sort_on)
+    return chars
